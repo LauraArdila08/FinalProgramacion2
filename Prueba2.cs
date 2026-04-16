922c678154ef9df10820304c9d4b2b0e19e1b604
LauraArdila08 <laura.ardilaa@upb.edu.co>
2026-04-16 10:21:24 -0500

    Prueba2Actualizada

---
+++ Prueba2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParcialFinal_ProgramacionModular
+{
+    internal class Prueba2
+    {
+        public void MostrarMensaje()
+        {
+            Console.WriteLine("Hola");
+        }
+    }
+}
